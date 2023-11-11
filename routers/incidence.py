4371aae5b220690d9bd8e0ff18d9cbf1f66d48d8
JoseLuisBarba <barba.farro.jose.luis@gmail.com>
2023-11-11 16:34:52 -0500

    Jose Luis: update incidence

--- routers/incidence.py
+++ routers/incidence.py
@@ -1,6 +1,6 @@
 
 from dtos.user import UserAuth, UserOut, UserUpdate
-from dtos.incidence import IncidenceCreate, IncidenceUpdate, IncidenceOut, IncidenceStatus
+from dtos.incidence import IncidenceCreate, IncidenceUpdate, IncidenceOut, IncidenceStatus, IncidenceLocationUpdate, IncidenceStatusUpdate
 from services.userService import UserService
 from services.incidenceService import IncidenceService
 from models.users import User
@@ -16,12 +16,17 @@ incidenceRouter = APIRouter()
 
 
 
-@incidenceRouter.post('/create', summary="Create new incidence", response_model=IncidenceOut)
-async def createIncidence(data: IncidenceCreate):
+@incidenceRouter.post('/me/create/', summary="Create new incidence", response_model=IncidenceOut)
+async def createIncidence(data: IncidenceCreate, user: User = Depends(getCurrentUser)):
     async with async_session() as session:
         async with session.begin():
             try:
-                return await IncidenceService(session).createIncidence(data)
+                if user is not None:
+                    return await IncidenceService(session).createIncidence(user.dni, data)
+                raise HTTPException(
+                    status_code= status.HTTP_404_NOT_FOUND,
+                    detail="User not found"
+                )
             except IntegrityError:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
@@ -62,6 +67,47 @@ async def getIncidenceStatusById(id: str):
         async with session.begin():
             try:
                 return await IncidenceService(session).getIncidenceStatusById(id)
+            except IntegrityError:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Invalid token",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+                
+                
+                
+@incidenceRouter.put('/incidence/{id}/update/location/', summary="update location current citizen", response_model= IncidenceOut)
+async def updateLocationIncidenceById(id: int, data: IncidenceLocationUpdate, user: User = Depends(getCurrentUser)):
+    async with async_session() as session:
+        async with session.begin():
+            try:
+                if user is not None:
+                    await IncidenceService(session).updateLocationIncidenceById(id, data)
+                    return await IncidenceService(session).getIncidenceById(id)
+                raise HTTPException(
+                    status_code= status.HTTP_404_NOT_FOUND,
+                    detail="User not found"
+                )
+            except IntegrityError:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Invalid token",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+                
+                
+@incidenceRouter.put('/incidence/{id}/update/status/', summary="update status current citizen", response_model= IncidenceOut)
+async def updateLocationIncidenceById(id: int, data: IncidenceStatusUpdate, user: User = Depends(getCurrentUser)):
+    async with async_session() as session:
+        async with session.begin():
+            try:
+                if user is not None:
+                    await IncidenceService(session).updateStatusIncidenceById(id,data)
+                    return await IncidenceService(session).getIncidenceById(id)
+                raise HTTPException(
+                    status_code= status.HTTP_404_NOT_FOUND,
+                    detail="User not found"
+                )
             except IntegrityError:
                 raise HTTPException(
                     status_code=status.HTTP_403_FORBIDDEN,
